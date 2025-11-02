c9956c249123fa8e6cdf13513eca0ba3ddf745cd
Wiktorlag <wiktorlag2003@wp.pl>
2025-11-02 21:59:24 +0100

    lab2

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,40 @@
 #pragma once
-
 #include "Resource.hpp"
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource* resource = nullptr;
+
+public:
+    double get() { return resource->get(); }
+
+    ResourceManager() : resource(new Resource()) {}
+
+    ~ResourceManager() { delete resource; }
+
+    ResourceManager(const ResourceManager& other) { resource = new Resource(*other.resource); }
+
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this != &other) {
+            delete resource;
+            resource = new Resource(*other.resource);
+        }
+        return *this;
+    }
+
+    ResourceManager(ResourceManager&& other)
+    {
+        resource       = other.resource;
+        other.resource = nullptr;
+    }
+
+    ResourceManager& operator=(ResourceManager&& other)
+    {
+        delete resource;
+        resource       = other.resource;
+        other.resource = nullptr;
+        return *this;
+    }
 };
